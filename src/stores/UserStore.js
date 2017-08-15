0fb3ba9f3d69fe409e2cf22f1665a041ba75dda8
Aldo Lugo <lugomatic@gmail.com>
2017-08-15 16:27:01 -0500

    added jsDocs to UserStore class

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -10,13 +10,20 @@ import axios from 'axios';
 //     return Promise.reject(error);
 // });
 
+
+/** Class representing a user operations. */
 class UserStore {
+    /** Collects user login credentials at the time of login. */
     @observable credentials = {
         email: '',
         password: ''
     };
     @observable validationError = '';
-
+    /**
+     * Login user
+     * @description - Login an already registred user back into the system.
+     * @param {function} callback - Callback function to be executed on a successful login.
+     */
     @action loginUser = (callback) =>{
         this.validationError = '';
         if(this.credentials.email !== '' || this.credentials.password !== ''){
@@ -27,7 +34,10 @@ class UserStore {
             });
         }
     }
-
+    /**
+     * Login user
+     * @description - Logout the user from the system and delete authToken from storage.
+     */
     @action logoutUser = () =>{
         this.apiAuth.signout(() => {
             console.log(`Logging out ${this.credentials.email}`);
@@ -35,10 +45,16 @@ class UserStore {
         });
     }
 
+    /** Keeps track of the current auth status that is used to protect routes. */
     @observable authStatus = {
         isAuthenticated: false
     }
     @observable apiAuth = {
+        /**
+         * Authenticate a user, this is called from loginUser.
+         * @description - Login an already registred user back into the system.
+         * @param {function} callback - Callback function to be executed on a successful login.
+         */
         authenticate: (cb) => {
             axios({
                 method: 'post',
@@ -65,12 +81,28 @@ class UserStore {
                 }
             });
         },
+        /**
+         * Logout a user, this is called from logoutUser.
+         * @description - Logout an already logged in user.
+         * @param {function} callback - Callback function to be executed on a successful login.
+         */
         signout: (cb)=> {
             axios.defaults.headers.common['Authorization'] = "";
             localStorage.clear('authToken');
             this.authStatus.isAuthenticated = false;
             cb();
          }
+    }
+
+
+
+    /**
+    * Register a new user
+    * @description - Register a new user with the system.
+    * @param {object} newUserData - New user form data. 
+    */
+    @action registerUser = (newUserData)=> {
+
     }
 
 }
